fcc985ea9072beac57e4b484dd1268e929a39733
Anthony Beacock <tonybeacock@hotmail.co.uk>
2019-04-15 00:01:37 +0100

    update

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerController : MonoBehaviour
 {
     [Header("Control Modifiers")]
-    public float speedMultiplier = 2f;
+    public float speedMultiplier = 5.0f;
     public float sidePushForce = 0.1f;
     public float turnRate = 2;
 
@@ -32,29 +32,55 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Time.timeScale == 1)
+        if (Time.time >= 20)
         {
-
-            if (Input.GetKey(KeyCode.A))
-            {
-                rb.AddForceAtPosition(new Vector3(-sidePushForce * Time.deltaTime, 0, 0), tf.position + new Vector3(0, 0.5f, 0));
-            }
-            if (Input.GetKey(KeyCode.D))
-            {
-                rb.AddForceAtPosition(new Vector3(sidePushForce * Time.deltaTime, 0, 0), tf.position + new Vector3(0, 0.5f, 0));
-            }
-
-            float angle = tf.eulerAngles.z;
-            if (angle > 180)
-            {
-                angle = 360 - angle;
-            }
-            else
-            {
-                angle = -angle;
-            }
-            tf.Translate(new Vector3(angle * turnRate * Time.deltaTime, 0, speedMultiplier * Time.deltaTime), Space.World);
+            speedMultiplier = 7.5f;
         }
+        if (Time.time >= 40)
+        {
+            speedMultiplier = 10.0f;
+        }
+        if (Time.time >= 60)
+        {
+            speedMultiplier = 12.5f;
+        }
+        if (Time.time >= 75)
+        {
+            speedMultiplier = 15.0f;
+        }
+        if (Time.time >= 90)
+        {
+            speedMultiplier = 17.5f;
+        }
+        if (Time.time >= 100)
+        {
+            speedMultiplier = 20.0f;
+        }
+
+
+        //if (Time.timeScale == 1)
+        //{
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForceAtPosition(new Vector3(-sidePushForce * Time.deltaTime, 0, 0), tf.position + new Vector3(0, 0.5f, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForceAtPosition(new Vector3(sidePushForce * Time.deltaTime, 0, 0), tf.position + new Vector3(0, 0.5f, 0));
+        }
+
+        float angle = tf.eulerAngles.z;
+        if (angle > 180)
+        {
+            angle = 360 - angle;
+        }
+        else
+        {
+            angle = -angle;
+        }
+        tf.Translate(new Vector3(angle * turnRate * Time.deltaTime, 0, speedMultiplier * Time.deltaTime), Space.World);
+        //}
     }
 
     private void OnCollisionEnter(Collision collision)
--- Assets/Scripts/Sound.cs
+++ Assets/Scripts/Sound.cs
@@ -10,8 +10,8 @@ public class Sound : MonoBehaviour
     {
         get { return soundInstance; }
     }
-    // Use this for initialization
 
+    // Use this for initialization
     private void Awake()
     {
         if (soundInstance != null && soundInstance != this)
@@ -26,6 +26,38 @@ public class Sound : MonoBehaviour
 
 
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Update()
+    {
+        var audioSource = this.GetComponent<AudioSource>();
+        if (audioSource.clip.name == "MainGameSound")
+        {
+            if (Time.time >= 20)
+            {
+                audioSource.pitch = 1.05f;
+            }
+            if (Time.time >= 40)
+            {
+                audioSource.pitch = 1.1f;
+            }
+            if (Time.time >= 60)
+            {
+                audioSource.pitch = 1.15f;
+            }
+            if (Time.time >= 75)
+            {
+                audioSource.pitch = 1.2f;
+            }
+            if (Time.time >= 90)
+            {
+                audioSource.pitch = 1.25f;
+            }
+            if (Time.time >= 100)
+            {
+                audioSource.pitch = 1.3f;
+            }
+        }
     }
 
 }
